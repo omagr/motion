e228097789b96dfbee9ad57471dbe08ee02cd6e5
om agarwal <omag.about@gmail.com>
2022-05-16 01:17:33 +0530

    version 1.5

--- back-end/app.js
+++ back-end/app.js
@@ -3,13 +3,13 @@ import NoteAPI from "./noteAPI.js";
 import NoteUI from "./noteUI.js"
 
 export default class App {
-    constructor (root) {
+    constructor(root) {
         this.notes = [],
-        this.active_notes = null,
-        this.view = new NoteUI(root, this._handlers());
+            this.active_notes = null,
+            this.view = new NoteUI(root, this._handlers());
         this._refresh_note();
     }
-    _refresh_note () {  
+    _refresh_note() {
         // get all notes 
         const notes_array = NoteAPI.get_all_notes();
         // it will call the ui to update screen and show our notes 
@@ -25,20 +25,20 @@ export default class App {
         this.view._update_note_tabs(notes_array);
         this.view._check_visibility_for_screen_ui(notes_array.length > 0);
     }
-    _setActiveNote (that_active_single_note) {
+    _setActiveNote(that_active_single_note) {
         this.active_notes = that_active_single_note;
         this.view._update_screen_ui(that_active_single_note);
     }
-    _handlers () {
+    _handlers() {
         return {
             // i need to select that note which is set as selected note 
-            on_note_select : (that_note_id) => {
+            on_note_select: (that_note_id) => {
                 // find that note fron note array which we give from screen UI 
-                const selected_note = this.notes.find(that_note => that_note.id == that_note_id )
+                const selected_note = this.notes.find(that_note => that_note.id == that_note_id)
                 // set that finded note to active note 
                 this._setActiveNote(selected_note)
             },
-            on_note_add : () => {
+            on_note_add: () => {
                 const new_note = {
                     title: "",
                     body: ""
@@ -46,7 +46,7 @@ export default class App {
                 NoteAPI.save_current_note(new_note);
                 this._refresh_note();
             },
-            on_note_edit : (new_title, new_body) => {
+            on_note_edit: (new_title, new_body) => {
                 NoteAPI.save_current_note({
                     id: this.active_notes.id,
                     title: new_title,
@@ -54,7 +54,7 @@ export default class App {
                 })
                 this._refresh_note();
             },
-            on_note_delete : (that_note_id) => {
+            on_note_delete: (that_note_id) => {
                 NoteAPI.delete_current_note(that_note_id);
                 this._refresh_note();
             }
--- back-end/noteAPI.js
+++ back-end/noteAPI.js
@@ -1,14 +1,14 @@
 export default class noteAPI {
-    static get_all_notes () {
-        const notes_array = JSON.parse(localStorage.getItem("motion_notes") || "[]" )
+    static get_all_notes() {
+        const notes_array = JSON.parse(localStorage.getItem("motion_notes") || "[]")
         // sorting algorithim
-        return notes_array.sort((a,b) => { return new Date(a.updated) > new Date(b.updated) ? -1 : 1});
+        return notes_array.sort((a, b) => { return new Date(a.updated) > new Date(b.updated) ? -1 : 1 });
     }
-    static save_current_note (noteToSave) {
+    static save_current_note(noteToSave) {
         // taking notes as a refrence 
         const notes_array = noteAPI.get_all_notes();
         // find the current note id with all the existing note ids 
-        const exist_note = notes_array.find(note_id => note_id == noteToSave.id)
+        const exist_note = notes_array.find(note_id => note_id.id == noteToSave.id)
         // if it finds then 
         if (exist_note) {
             // update note title with the current note title 
@@ -17,26 +17,26 @@ export default class noteAPI {
             exist_note.body = noteToSave.body;
             // update note date with the current note date 
             exist_note.updated = new Date().toISOString();
-        } 
+        }
         // otherwise 
         else {
-        // updating the notes from the parameter 
-        noteToSave.id = Math.floor(Math.random() * 1000000);
-        noteToSave.updated = new Date().toISOString();
-        // updating notes array 
-        notes_array.push(noteToSave)
+            // updating the notes from the parameter 
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            // updating notes array 
+            notes_array.push(noteToSave)
         }
         // updating the stoirage key motion-notes 
         localStorage.setItem("motion_notes", JSON.stringify(notes_array))
     }
-    static delete_current_note (id) {
+    static delete_current_note(id) {
         // taking notes as a refrence 
         const notes_array = noteAPI.get_all_notes();
         // delete that note by the mehtod of filter 
         /*
         this will remove that current note and store all other notes in update note array
         */
-        const updated_notes_array =  notes_array.filter(every_note => every_note.id != id);
+        const updated_notes_array = notes_array.filter(every_note => every_note.id != id);
         // updating the stoirage key motion-notes 
         localStorage.setItem("motion_notes", JSON.stringify(updated_notes_array))
     }
